ad3888d2169534231b5a3ee689d9f5e082831452
Alonsont <alo.reyesd@duocuc.cl>
2024-09-23 18:30:48 -0300

    link de boton agregar vehiculo

--- src/app/pages/perfil/perfil.page.ts
+++ src/app/pages/perfil/perfil.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-perfil',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+  navigateToRegistroVehiculo() {
+    this.router.navigate(['/registro-vehiculo'])
+  }
 }
