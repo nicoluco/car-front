eb28a447c60eab9b9bdc52f91d5d2030e2ea6af9
fepardo <fe.pardob@duocuc.cl>
2024-09-26 16:43:47 -0300

    añadido historial
    añadido el for que recorre la tabla historial para llenar la pantalla con los historiales del vehiculo del usuario

--- src/app/pages/historial/historial.page.ts
+++ src/app/pages/historial/historial.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { APIService } from '../../api.service';
 
 @Component({
   selector: 'app-historial',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HistorialPage implements OnInit {
 
-  constructor() { }
+  entradas_mantenimientos: any[] = [];
+  entrada_mantenimiento: String;
+
+  constructor(private apiService: APIService) { 
+
+    this.entrada_mantenimiento= "";
+  }
 
   ngOnInit() {
+
+    const tipo = "HistorialMantenimientos/"
+    this.apiService.getData(tipo, true).subscribe(response => {
+      console.log('mantenimientos recibidos:', response);
+      this.entradas_mantenimientos = response
+    }, error => {
+      console.error('Error al hacer la petición:', error);
+    });
   }
 
 }
